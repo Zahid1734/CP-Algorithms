de06982d87f5fd27de07d34677d75c0b7e56667c
Zahidul Islam <zahidcdt8@gmail.com>
2022-11-15 22:49:25 +0600

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -17,6 +17,7 @@ using int128 = __int128_t;
 #define ff first
 #define PI acos(-1)
 #define setbit(a, b) (a |= (1LL << b))
+#define checkbit(a, b) (a & (1LL << b))
 #define countbit(a) __builtin_popcount(a) // use __builtin_popcountll for long long
 #define erasebit(a, b) (a &= (~(1LL << b)))
 #define reversebit(a, b) (a ^= (1LL << b))
@@ -26,33 +27,20 @@ using int128 = __int128_t;
 // int is the type of the data being stored. It can be float, pair etc
 // find_by_order(x) -> returns the (x + 1) th element in the set
 // order_of_key(x) -> number of items strictly smaller than x
-
 const ll INF = (1LL << 63);
 
-void solve( ) {
-    int a;
-    a = 15;
-    int ones = countbit(a);
-    cout << ones << "\n";
-    erasebit(a, 1);
-    cout << a << "\n";
-    setbit(a, 1);
-    cout << a << "\n";
-    reversebit(a, 2);
-    cout << a << "\n";
+void solve() {
+
 }
 
-int main(void) {
-  ios_base :: sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
+int main() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL);
 
-  int cases = 1;
-  //cin >> cases;
-
-  for (int i = 1; i <= cases; i++) {
-    //cout << "Case " << i << ": ";
-    solve();
-  }
-
-  return 0;
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) {
+        //cout << "Case " << i << ": ";
+        solve();
+    }
 }
