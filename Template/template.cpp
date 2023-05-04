285da6c46a2ba0a16d2642539bce0d45b6c001df
Zahidul Islam <51478632+Zahid1734@users.noreply.github.com>
2023-05-04 14:25:24 +0600

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,19 +1,26 @@
-#pragma GCC target ("avx2")
+#pragma GCC target("avx2")
 #pragma GCC optimization ("O3")
 #pragma GCC optimization ("unroll-loops")
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
+
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef ZAHID_LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
 using ll = long long;
 using ull = unsigned long long;
-using int128 = __int128_t;
 
 #define pb push_back
-#define second ss
+#define ss second
 #define ff first
 #define PI acos(-1)
 #define setbit(a, b) (a |= (1LL << b))
@@ -21,24 +28,22 @@ using int128 = __int128_t;
 #define countbit(a) __builtin_popcount(a) // use __builtin_popcountll for long long
 #define erasebit(a, b) (a &= (~(1LL << b)))
 #define reversebit(a, b) (a ^= (1LL << b))
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-// less<int> is the comparing function
-// null_type means it is being used as a set
-// int is the type of the data being stored. It can be float, pair etc
-// find_by_order(x) -> returns the (x + 1) th element in the set
-// order_of_key(x) -> number of items strictly smaller than x
+#define sz(a) (int)a.size()
+
 const ll INF = (1LL << 63);
 
-void solve() {
 
+void solve() {
+    
+    
 }
 
 int main() {
     ios_base::sync_with_stdio(NULL);
     cin.tie(NULL);
-
-    int tt;
-    cin >> tt;
+    
+    int tt = 1;
+    //cin >> tt;
     for (int i = 1; i <= tt; i++) {
         //cout << "Case " << i << ": ";
         solve();
