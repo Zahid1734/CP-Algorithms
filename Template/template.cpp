9eefee73347b585777f7161c3b30492ad0dc6b15
Zahidul Islam <51478632+Zahid1734@users.noreply.github.com>
2023-05-13 23:56:42 +0600

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,9 +1,14 @@
-#pragma GCC target("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+
+#ifdef ZAHID_LOCAL
+    #define _GLIBCXX_DEBUG
+#endif
+
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
 template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
@@ -19,22 +24,24 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 using ll = long long;
 using ull = unsigned long long;
 
-#define pb push_back
-#define ss second
+#define eb emplace_back
+#define second ss
 #define ff first
 #define PI acos(-1)
 #define setbit(a, b) (a |= (1LL << b))
 #define checkbit(a, b) (a & (1LL << b))
 #define countbit(a) __builtin_popcount(a) // use __builtin_popcountll for long long
+#define sz(a) int(a.size())
 #define erasebit(a, b) (a &= (~(1LL << b)))
 #define reversebit(a, b) (a ^= (1LL << b))
-#define sz(a) (int)a.size()
-
-const ll INF = (1LL << 63);
-
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+// less<int> is the comparing function
+// null_type means it is being used as a set
+// int is the type of the data being stored. It can be float, pair etc
+// find_by_order(x) -> returns the (x + 1) th element in the set
+// order_of_key(x) -> number of items strictly smaller than x
 
 void solve() {
-    
     
 }
 
