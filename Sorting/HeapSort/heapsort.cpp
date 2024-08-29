438f663c81e82f6d8ce71e1cd6fa54626867ea04
Zahid1734 <zahidcdt8@gmail.com>
2024-08-29 20:02:29 +0600

    Included Heap Sort

--- Sorting/HeapSort/heapsort.cpp
+++ Sorting/HeapSort/heapsort.cpp
@@ -16,21 +16,32 @@ public:
     // sorts array a from index ll to index rr
     HeapSort(vector<int> &a, int ll, int rr) {
         int n = a.size();
-        int l = ll, r = rr;
+        l = ll, r = rr;
         if (l > r || r >= n) return ;
-        
+
+        // creating the heap in O(n) instead of O(nlogn)
+        for (int i = r; i >= l; i--) {
+            ShiftDown(a, i - l);
+        }
+        //Sorting the vector using the heap property
+        dbg(a);
+        while(l <= r) {
+            swap(a[l], a[r]);
+            r--;
+            ShiftDown(a, 0);
+        }
     }
 
     void ShiftDown(vector<int> &a, int id) {
-        while(id <= r) {
+        while(id <= r - l) {
             int left = 2 * id + 1;
             int right = 2 * id + 2;
 
-            if (left > r) return ;
+            if (left > r - l) return ;
             int min_id = left;
-            if (right <= r && a[right] < a[left]) min_id = right; 
-            if (a[id] <= a[min_id]) return ;
-            swap(a[id], a[min_id]);
+            if (right <= r - l && a[right + l] < a[left + l]) min_id = right; 
+            if (a[id + l] <= a[min_id + l]) return ;
+            swap(a[id + l], a[min_id + l]);
             id = min_id;
         }
     }
@@ -38,7 +49,12 @@ public:
 
 int main() {
 
-    
+    vector<int> a = {5, 4, 3, 1, 2};
+
+    // Usage -> (vector to sort, left border, right border) indexing is zero based
+    HeapSort heap(a, 2, 4);
+
+    for (int i = 4; i >= 2; i--) cout << a[i] << " ";
 
     return 0;
 }
